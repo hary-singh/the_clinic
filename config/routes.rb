508093db704aedacd209f2b024388f485f14f8ff
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-11 21:08:18 -0700

    routes for user updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
 
   root 'users#index'
 
-  resources :users 
-  resources :doctors do
+  resources :users do 
     resources :appointments
   end
   
+  resources :doctors do
+    resources :appointments
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
