7c2651214bffb0085715900a95b1f14e22d7a3f5
Jayden <jaydenowens23@gmail.comgit config --global core.editor vimtouch ~/.gitignorecode ~/.gitignoregit config --global core.excludesfile ~/.gitignore>
2021-03-11 20:17:20 -0700

    appointment controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,47 @@
 class AppointmentsController < ApplicationController
-  def index
+  before_action :set_user
+  before_action :set_doctor
+  def index 
+    @appointments = @users.doctor.appointments
+     render component: "Appointments", props: { user: @user, doctor: @doctor, appointments: @appointments}
   end
 
   def new
+    @appointment = @users.doctor.appointment.new
+    @users = User.all - @doctor.users
+    render component: "AppointmentNew", props: { user: @user, doctor: @doctor, appointment: @appointment}
   end
+  def create
+    @appointment = @users.doctor.appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to user_doctor_appoinments_path(@doctor)
+    else
+      @user = User.all - @doctor.users
+      render component: "AppointmentNew", props: {  user: @user, doctor: @doctor, appointment: @appointment}
+    end
+
+  end
+
+  def destroy
+    @appointment = @doctor.appointments.find(params[:id])
+    @appointment.destroy
+    redirect_to user_doctor_appoinments_path(@doctor)
+  end
+
+  private
+
+    def set_doctor
+      @doctor = Doctor.find(params[:doctor_id])
+    end
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def appointment_params
+      params.require(:appointment).permit(:time)
+    end
+
+
+
 end
