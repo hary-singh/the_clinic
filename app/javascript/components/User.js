3472b1f3cfbf787d239362ae4c0926c305991128
LuLu DeHaan <luludehaan@gmail.com>
2021-03-11 19:12:12 -0800

    user controller

---
+++ app/javascript/components/User.js
@@ -0,0 +1,15 @@
+import React from "react";
+const User = ({ user }) => {
+  const { id, first_name, Last_name } = user
+  return (
+    <>
+      <h1>Name: {Last_name}, {first_name}</h1>
+        <hr />
+        <a href={`/users/${id}`} data-method="delete">Delete User</a>
+        <br />
+        <a href={'/users'}> Back </a>
+     
+    </>
+  )
+}
+export default User;
--- app/javascript/components/Users.js
+++ app/javascript/components/Users.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Users = ({ users }) => {
-  return (
+  return(
     <>
-      <h1> Test User</h1>
+      <h1>Users</h1>
+      <a href={`/users/new`}>New User</a>
+      { users.map( (u) => (
+        <div>
+          <a href={`/users/${u.id}`}>{u.first_name} {u.Last_name}</a>
+        </div>
+      ))}
     </>
   )
 }
-
 export default Users;
