a2244c30b0b8a73035ce643375478bee246d9195
Jayden <jaydenowens23@gmail.comgit config --global core.editor vimtouch ~/.gitignorecode ~/.gitignoregit config --global core.excludesfile ~/.gitignore>
2021-03-14 22:28:47 -0600

    controller and component appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,31 +1,30 @@
 class AppointmentsController < ApplicationController
-  before_action :set_user
+  # before_action :set_user
   before_action :set_doctor
   def index 
-    @appointments = @user.doctor.appointments
-     render component: "Appointments", props: { user: @user, doctor: @doctor, appointments: @appointments}
+    @appointments = @doctor.appointments
+     render component: "Appointments", props: { doctor: @doctor, appointments: @appointments}
   end
 
   def new
-    @appointment = @user.doctor.appointment.new
+    @appointment = @doctor.appointments.new
     @users = User.all - @doctor.users
-    render component: "AppointmentNew", props: { user: @user, doctor: @doctor, appointment: @appointment}
+    render component: "AppointmentNew", props: { doctor: @doctor, appointment: @appointment}
   end
   def create
-    @appointment = @user.doctor.appointment.new(appointment_params)
+    @appointment = @doctor.appointments.new(appointment_params)
     if @appointment.save
-      redirect_to user_doctor_appoinments_path(@doctor)
+      redirect_to doctor_appoinments_path(@doctor)
     else
-      @user = User.all - @doctor.users
-      render component: "AppointmentNew", props: {  user: @user, doctor: @doctor, appointment: @appointment}
+      render :new
     end
 
   end
 
   def destroy
-    @appointment = @user.doctor.appointments.find(params[:id])
+    @appointment = @doctor.appointments.find(params[:id])
     @appointment.destroy
-    redirect_to user_doctor_appoinments_path(@doctor)
+    redirect_to doctor_appoinments_path(@doctor)
   end
 
   private
@@ -34,9 +33,9 @@ class AppointmentsController < ApplicationController
       @doctor = Doctor.find(params[:doctor_id])
     end
 
-    def set_user
-      @user = User.find(params[:user_id])
-    end
+    # def set_user
+    #   @user = User.find(params[:user_id])
+    # end
 
     def appointment_params
       params.require(:appointment).permit(:time)
