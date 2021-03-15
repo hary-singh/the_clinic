4f996b51fa70f1d0125c2a372f19bd6b7aadc562
Jayden <jaydenowens23@gmail.comgit config --global core.editor vimtouch ~/.gitignorecode ~/.gitignoregit config --global core.excludesfile ~/.gitignore>
2021-03-14 21:45:18 -0600

    contoller on appointments

--- app/javascript/components/AppointmentNew.js
+++ app/javascript/components/AppointmentNew.js
@@ -1,42 +1,50 @@
 import React from 'react';
-import Appointments from './Appointments';
 
-const AppointmentNew = ({doctor, user}) => {
-    const { time } = Appointments
-    const defaultTime = time ? time : "";
-    const defaultDoctor = doctor_id ? doctor_id : "";
-    return(
-        <>
-            <h1> Add Appoinments </h1>
-            { errors && errors }
-            <form action={'/doctors/${doctor.id}/ appointments'} method="post">
-                <label for="role">Choose a Doctor</label>
-                <select
-                    time='appointment[time]'
-                    id= 'doctor_id'
-                    name= 'user'
-                    defaultValue={defaultDoctor}
-                >
-                    <option value="doctors">Doctors</option>
-
-                </select>
-                <br />
-                <label for="time"> Choose Your Time</label>
-                <select
-                    time="appointment[time]"
-                    id= "user_id"
-                    defaultValue={defaultUser}
-                >
-                    {users.map((u)=> (
-                        <option value = {u.id}>
-                            {'${u.first_name} ${u.last_name}'}
-                        </option>
-
-                    ))}
-                </select>
-                <button type="submit">Submit</button>
-            </form>
-        </>
-    )
+const AppointmentNew = ({ doctor, time, user}) => {
+  const { doctor, errors, user, time } = enrollment;
+  const defaultDoctor = doctor ? doctor : "";
+  const defaultUser = user ? user : "";
+  const defaultTime = time ? time : "";
+  return(
+    <>
+      <h1>Add Doctor</h1>
+      { errors && errors }
+      <form action={`/doctor/${doctor.id}/appointments`} method="post">
+        <label for="doctor">Choose a Doctor:</label>
+        <input 
+          name="appointment[doctor]" 
+          id="doctor" 
+          defaultValue={defaultDoctor}
+          type = 'text'
+          >
+        </input>
+        <br />
+        <label for="user">What is your name?:</label>
+        <input 
+          name="appointment[user]" 
+          id="user" 
+          defaultValue={defaultUser}
+          type = "text"
+          >
+        </input>
+        <label for="Time">What time would you like to see the Doctor?</label>
+        <input 
+          name="appointment[time]" 
+          id="user" 
+          defaultValue={defaultTime}
+          type = "text"
+          >
+        </input>
+      { users.map((u) => (
+        <option value={u.id}>
+          {`${u.first_name} ${u.last_name}`}
+        </option>
+          ))}
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  )
 }
-export default AppointmentNew
+
+
+export default AppointmentNew;
--- app/javascript/components/LandingPage.js
+++ app/javascript/components/LandingPage.js
@@ -4,16 +4,9 @@ const LandingPage = () => {
   return (
     <>
     <h1> Welcome to our clinic.</h1><br/>
-<<<<<<< HEAD
     <h3> Please select one of the options to proceed:</h3><hr/><br /><br />
     <div align="center"><a href="/doctors" class="btn waves-effect waves-light"> Doctors</a><br /><br />
     <a href="/users" class="btn waves-effect waves-light"> Users</a><br/></div>
-=======
-    <h3> Please select one of the options to proceed:</h3><hr/>
-    <a href="/doctors"> Doctors</a><br/>
-    <a href="/users"> Users</a><br/>
-    <a href="/appoinments"> Appointments</a>
->>>>>>> 0cea59d (added JS need to debug now)
     </>
   )
 }
